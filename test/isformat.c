7200bf952f7f6a859335a35a026fa6ffd2ed3054
Marwa_Sabon <marwasabon@gmail.com>
2023-07-24 03:29:06 -0700

    format file

---
+++ test/isformat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+/**
+ * isformatvalid - function to print the format string, and with respective
+ * formatting specifier.
+ * @format: this is the string containing the format and string to print
+ *
+ * Return: true for invalid false for valid format
+ */
+bool isformatvalid(const char *format)
+{
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+				case 'd':
+				case 'i':
+				case 'o':
+				case 'u':
+				case 'x':
+				case 'X':
+				case 'f':
+				case 'e':
+				case 'E':
+				case 'g':
+				case 'G':
+				case 'a':
+				case 'A':
+				case 's':
+				case 'p':
+				case '%':
+					{
+						break;
+					}
+				default:
+					{
+						return (false);
+					}
+			}
+		}
+		format++;
+	}
+	return (true);
+}
